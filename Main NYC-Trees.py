208773a5969d0438a3f502f9fd8dba3cb2ad92e5
Jacques Declercq <41050093+JacquesDeclercq@users.noreply.github.com>
2021-07-04 21:36:27 +0200

    Last change

--- Main NYC-Trees.py
+++ Main NYC-Trees.py
@@ -4,6 +4,7 @@ file = '2015_Street_Tree_Census_-_Tree_Data.csv'
 df_original = pd.read_csv(file, sep =",")
 df_copy = df_original.copy()
 
+
 strip_strings_in_whole_dataset(df_copy)
 strings_to_lower_in_whole_dataset(df_copy)
 
@@ -19,10 +20,14 @@ convert_steward_to_string_of_numbers(df_copy)
 
     # fix nan for column health for one alive tree
 fill_nan_health_alive_tree(df_copy)
+df_test = df_copy.copy()
 
+    #slice the df to a smaller piece
+
+df_copy = df_copy.iloc[:125000,:]
 
 #def create_csv_file():
-df_copy.to_csv('NYC_trees_XL', sep=',', encoding='utf-8')
+df_copy.to_csv('NYC_trees_S', sep=',', encoding='utf-8')
 
 
 print(df_copy.info())
